48fd90a4f5c842bc59efb3df57f838272f6bc69d
front-of-ak <artemhub06@yandex.ru>
2022-12-09 20:46:50 +0300

    button class template

---
+++ widgets/pygame_button.py
@@ -0,0 +1,26 @@
+class Button:
+    def __init__(self, width=50, height=20, border_width=1, border_radius=0,
+                 outline="black", colour="black", text="", text_colour="black", img=""):
+        pass
+
+    def __click(self):
+        pass
+
+    def __hover(self):
+        pass
+
+    def destroy(self):
+        pass
+
+    def create(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def hide(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
