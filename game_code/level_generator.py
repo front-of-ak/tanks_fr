4152dc30df9e994aacff98845de23c8afccb9e31
front-of-ak <artemhub06@yandex.ru>
2022-12-10 21:59:58 +0300

    second and current vers level_generator.py

--- game_code/level_generator.py
+++ game_code/level_generator.py
@@ -1,45 +1,43 @@
 import os
 import random
 
-OBJECTS = ['.', '/', '-', '@']
+OBJECTS = ['.', '/', '-', '@', '&']  # . - grass, / - wall, - - enemy, @ - player, & - smth
+MAP_HEIGHT, MAP_WIDTH = 20, 27
+
+
+def count_elems(inp_list, element):
+    return sum([i.count(element) for i in inp_list])
+
+
+# def wall_around(inp_list):
+#     for i in range(len(inp_list)):
+#         for j in range(len(inp_list[i])):
+#             try:
+#                 if inp_list[i - 1][j - 1] == '/':
+#                     pass
+#             except IndexError:
 
 
 def generate(level_name):
-    level_map = []
-    enemies = 0
-    for i in range(20):
-        level_map.append([])
-        areHouses = random.choice((True, False, True, True))
-        areEnemies = random.choice((False, False, True))
-        houses_positions = []
-        enemies_positions = []
-        if areHouses:
-            houses_amount = random.randint(0, 15)
-            for _ in range(houses_amount):
-                temp_pos = random.randint(0, 27)
-                while temp_pos in houses_positions:
-                    temp_pos = random.randint(0, 27)
-                houses_positions.append(temp_pos)
-        if areEnemies and enemies <= 5:
-            enemies_amount = random.randint(1, 2)
-            enemies += 1
-            for _ in range(enemies_amount):
-                temp_pos = random.randint(0, 27)
-                while temp_pos in enemies_positions and temp_pos in houses_positions:
-                    temp_pos = random.randint(0, 27)
-                enemies_positions.append(temp_pos)
-        for j in range(27):
-            if i == 0 and j == 0:
-                level_map[i].append('@')
-            elif j in houses_positions:
-                level_map[i].append('/')
-            elif j in enemies_positions:
-                level_map[i].append('-')
-            else:
-                level_map[i].append('.')
+    level_map = [['.'] * MAP_WIDTH for i in range(MAP_HEIGHT)]
+    while count_elems(level_map, OBJECTS[1]) < (MAP_HEIGHT * MAP_WIDTH) * 0.2:
+        temp_x, temp_y = random.randint(0, MAP_WIDTH - 1), random.randint(0, MAP_HEIGHT - 1)
+        level_map[temp_y][temp_x] = OBJECTS[1]
+        # walls delete
+    while count_elems(level_map, OBJECTS[2]) < count_elems(level_map, OBJECTS[1]) * 0.07:
+        temp_x, temp_y = random.randint(0, MAP_WIDTH - 1), random.randint(0, MAP_HEIGHT - 1)
+        if level_map[temp_y][temp_x] == '.':
+            level_map[temp_y][temp_x] = OBJECTS[2]
+    while True:
+        temp_x, temp_y = random.randint(0, MAP_WIDTH - 1), random.randint(0, MAP_HEIGHT - 1)
+        if level_map[temp_y][temp_x] == '.':
+            level_map[temp_y][temp_x] = '@'
+            break
+    # while count_elems(level_map, OBJECTS[4]) < count_elems(level_map, OBJECTS[0]) * 0.3:
+    #     temp_x, temp_y = random.randint(0, MAP_WIDTH - 1), random.randint(0, MAP_HEIGHT - 1)
+    #     if level_map[temp_y][temp_x] == '.':
+    #         level_map[temp_y][temp_x] = '&'
     with open(os.path.join('data', 'level_maps', level_name), mode='wt', encoding='utf-8') as f:
         for i in level_map:
             f.write(''.join(i) + '\n')
     return level_name
-
-
